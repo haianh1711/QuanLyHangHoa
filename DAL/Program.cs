4ec5a785e8d6e5e5a3a441cae6f62429ea416113
Administrator <Administrator@MIO>
2025-02-10 10:34:33 +0700

    het loi

--- DAL/Program.cs
+++ DAL/Program.cs
@@ -1,8 +1,2 @@
-﻿using DAL;
-using DTO;
-
-TaiKhoanDTO? taiKhoanDTO = new TaiKhoanDTO();
-taiKhoanDTO = new DangNhapDAL().DangNhap("admin", "111");
-Console.WriteLine(taiKhoanDTO);
-Console.WriteLine("taiKhoanDTO");
-
+﻿Console.WriteLine("Hello World!");
+Console.ReadLine();
