6d8c6bef51b8eda34fcc2755aeabd59950e4ad14
PhhHuynn <phuonghuyen5872@gmail.com>
2025-02-12 00:41:55 +0700

    sửa connectionString

--- DAL/DatabaseHelper.cs
+++ DAL/DatabaseHelper.cs
@@ -10,7 +10,7 @@ namespace DAL
 {
     public class DatabaseHelper
     {
-        private string connectionString = "Data Source=.;Initial Catalog=QlBanHang;Integrated Security=True;Trust Server Certificate=True;";
+        private string connectionString = "Data Source=.;Initial Catalog=QlHangHoa;Integrated Security=True;Trust Server Certificate=True;";
 
         public SqlConnection GetSqlConnection()     
         {
