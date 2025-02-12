620121252574ba186ff38d5eb3084a314c9ed0c5
PhhHuynn <phuonghuyen5872@gmail.com>
2025-02-13 05:32:13 +0700

    liên kế main form

--- BLL/NhanVienBLL.cs
+++ BLL/NhanVienBLL.cs
@@ -13,25 +13,5 @@ namespace BLL
     internal class NhanVienBLL
     {
 
-        public bool GuiMail(string senderEmail, string senderPassword, string recipientEmail, string subject, string body)
-        {
-            
-                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587)
-                {
-                    // Password là mật khẩu ứng dụng (không phải mật khẩu chính của google)
-                    Credentials = new NetworkCredential(senderEmail, senderPassword),
-                    EnableSsl = true
-                };
-
-                MailMessage mailMessage = new MailMessage(senderEmail, recipientEmail)
-                {
-                    Subject = subject,
-                    Body = body
-                };
-
-                smtpClient.Send(mailMessage);
-                Console.WriteLine("Email đã gửi thành công!");
-                return true;
-        }
     }
 }
--- GUI/ViewModels/NhanvienViewModel.cs
+++ GUI/ViewModels/NhanvienViewModel.cs
@@ -7,16 +7,127 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using DTO;
+using BLL;
+using CommunityToolkit.Mvvm.Input;
+using GUI.ViewModels.UserControls;
+using GUI.Views.UserControls;
 
 namespace GUI.ViewModels
 {
     internal partial class NhanvienViewModel : ObservableObject
     {
         [ObservableProperty]
-        private ObservableCollection<NhanVienDTO> data;
+        private ThongBaoViewModel thongBaoVM = new ThongBaoViewModel();
+
+        private NhanVienBLL hangHoaBLL = new();
+
+        // dataGrid
+        [ObservableProperty]
+        private ObservableCollection<NhanVienDTO> hangHoaDTOs = [];
+
+        // Thuộc tính của phieu nhạp
+        [ObservableProperty]
+        private NhanVienDTO? selectedNhanVien;
+
+        // Tìm kiếm
+        [ObservableProperty]
+        private string? tuKhoaTimKiem;
+
         public NhanvienViewModel()
         {
-            
+            LoadDanhSachNhanVien();
+            SelectedNhanVien = new();
+        }
+
+        private void LoadDanhSachNhanVien()
+        {
+            NhanVienDTOs.Clear();
+            NhanVienDTOs = new ObservableCollection<NhanVienDTO>(BLL.HienThiDanhSachHH());
+        }
+
+        [RelayCommand]
+        private async Task ThemNhanVien()
+        {
+            try
+            {
+                if (SelectedNhanVien != null)
+                {
+
+                    bool result = hangHoaBLL.ThemNhanVien(SelectedNhanVien);
+                    if (result)
+                    {
+                        await ThongBaoVM.MessageOK("Thêm hàng hóa thành công");
+                        LoadDanhSachNhanVien();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await ThongBaoVM.MessageOK(ex.ToString());
+            }
+        }
+
+        [RelayCommand]
+        private async Task SuaNhanVien()
+        {
+            try
+            {
+                if (SelectedNhanVien != null)
+                {
+                    bool result = hangHoaBLL.CapnhatNhanVien(SelectedNhanVien);
+                    if (result)
+                    {
+                        await ThongBaoVM.MessageOK("Sửa hàng hóa thành công");
+                        LoadDanhSachNhanVien();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await ThongBaoVM.MessageOK(ex.ToString());
+            }
+        }
+
+        [RelayCommand]
+        private async Task XoaNhanVien()
+        {
+            try
+            {
+                if (SelectedNhanVien != null)
+                {
+                    bool isXoaPhieuNhap = await ThongBaoVM.MessageYesNo("Bạn có chắc chắn muốn xóa phiếu nhập này? Dữ liệu sẽ bị mất vĩnh viễn.");
+                    if (isXoaPhieuNhap)
+                    {
+                        bool result = hangHoaBLL.XoaNhanVien(SelectedNhanVien.MaHang);
+                        if (result)
+                        {
+                            await ThongBaoVM.MessageOK("Xóa phiếu nhập thành công");
+                            LoadDanhSachNhanVien();
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await ThongBaoVM.MessageOK(ex.ToString());
+            }
+
+        }
+
+        [RelayCommand]
+        private async Task TimKiem()
+        {
+            if (SelectedNhanVien != null)
+            {
+                TuKhoaTimKiem = TuKhoaTimKiem ?? "";
+                NhanVienDTOs = new ObservableCollection<NhanVienDTO>(hangHoaBLL.TimNhanVien(TuKhoaTimKiem));
+
+
+            }
+
         }
     }
 }
